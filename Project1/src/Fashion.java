8e9d194977324def636f8fb46f17830bc3238d6e
lelam1899 <kylix.par@gmail.com>
2019-02-16 14:04:09 +0700

    s,c

---
+++ Project1/src/Fashion.java
@@ -0,0 +1,9 @@
+public class Fashion {
+    public int Id;
+    public String name;
+    public double price;
+    public int quantity;
+    public String color;
+    public String size;
+    public String brand;
+}
---
+++ Project1/src/Product.java
@@ -0,0 +1,13 @@
+public class Product {
+    public int id;
+    public String name;
+    public double price;
+    public int quantity;
+
+    public Product(int id, String name, int price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
